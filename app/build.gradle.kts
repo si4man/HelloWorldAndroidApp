0d0767b137dbb8859131e1143ce7b9b6c476c336
night <authorityexceeded@gmail.com>
2025-10-23 22:29:31 +0300

    homework with lyrics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.gson)
+    implementation(libs.fancytoast)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
--- app/src/main/java/web/knight/helloworld/MainActivity.java
+++ app/src/main/java/web/knight/helloworld/MainActivity.java
@@ -1,6 +1,7 @@
 package web.knight.helloworld;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,7 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.shashank.sony.fancytoastlib.FancyToast;
+
 public class MainActivity extends AppCompatActivity {
+
+    private int lineCounter = 0;
+    private String[] lyrics;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        lyrics = getResources().getStringArray(R.array.song_lyrics);
+        Log.i(TAG, "onCreate");
     }
 
     public void changeWidth (View view) {
@@ -34,6 +44,59 @@ public class MainActivity extends AppCompatActivity {
         if (currentWidth == maxWidth) {
             mainButton.setText("Ты дошёл до конца!");
         }
+    }
+
+    public void showTextLyrics(View v) {
+        if (lyrics == null || lyrics.length == 0) return;
+
+        FancyToast.makeText(this, lyrics[lineCounter],FancyToast.LENGTH_LONG,FancyToast.INFO,false).show();
+        lineCounter = (lineCounter + 1) % lyrics.length;
+    }
+
+    String TAG = "life";
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        Log.i(TAG, "onCreate");
+//    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
     }
 
 }
